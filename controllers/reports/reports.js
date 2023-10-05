3fb8da597714e86a638e0a16068c88d1b20c64e5
Mike Igor Mourão da Silva <mike.silva@grupoicts.com.br>
2023-10-05 09:02:12 -0400

    feat: logic for production cost for keywords

--- controllers/reports/reports.js
+++ controllers/reports/reports.js
@@ -15,9 +15,33 @@ export const getProductById = (req, res) => {
         if (data.length === 0) {
             return res.status(404).json("Produto não encontrado.");
         }
-        
+
         if (data[0].type === "Normal") {
-            const manufacturingCost = (data[0].cost / 50 /* ajustar de acordo com a quantidade que dá pra produzir */)
+            function checkKeyword(str, keyWord) {
+                // Converte a string para minúsculas para tornar a comparação insensível a maiúsculas e minúsculas
+                const stringMinusc = str.toLowerCase();
+    
+                // Verifica cada palavra-chave na string
+                for (const keyWords of keyWord) {
+                    if (stringMinusc.includes(keyWords.toLowerCase())) {
+                        return true; 
+                    }
+                }
+                return false; 
+            }
+    
+            const myString = data[0].product_name;
+            const myKeyWords = ["fita"];
+            const myKeyWordsPaper = ["papel"];
+            const myKeyWordsGlue = ["cola"];
+
+    
+            const returnTape = checkKeyword(myString, myKeyWords);
+            const returnPaper = checkKeyword(myString, myKeyWordsPaper);
+            const returnGlue = checkKeyword(myString, myKeyWordsGlue);
+
+            
+            const manufacturingCost = (data[0].cost / (returnTape ? 10 : returnPaper ? 50 : returnGlue ? 100 : 20))
             const newData = [{
                 id: data[0].id,
                 product_name: data[0].product_name,
@@ -26,10 +50,32 @@ export const getProductById = (req, res) => {
                 size: data[0].size,
                 manufacturing_cost: manufacturingCost
             }]
-            return res.status(200).json(newData); // Retorna o resultado encontrado com novo valor de manufacturing_cost
+            return res.status(200).json(newData);
         }
 
-        const manufacturingCost = (data[0].cost / 10 /* ajustar de acordo com a quantidade que dá pra produzir */)
+        function checkKeyword(str, keyWord) {
+            // Converte a string para minúsculas para tornar a comparação insensível a maiúsculas e minúsculas
+            const stringMinusc = str.toLowerCase();
+
+            // Verifica cada palavra-chave na string
+            for (const keyWords of keyWord) {
+                if (stringMinusc.includes(keyWords.toLowerCase())) {
+                    return true; 
+                }
+            }
+            return false; 
+        }
+
+        const myString = data[0].product_name;
+        const myKeyWords = ["fita"];
+        const myKeyWordsPaper = ["papel"];
+        const myKeyWordsGlue = ["cola"];
+
+        const returnTape = checkKeyword(myString, myKeyWords);
+        const returnPaper = checkKeyword(myString, myKeyWordsPaper);
+        const returnGlue = checkKeyword(myString, myKeyWordsGlue);
+        
+        const manufacturingCost = (data[0].cost / (returnTape ? 10 : returnPaper ? 50 : returnGlue ? 100 : 20))
         const newData = [{
             id: data[0].id,
             product_name: data[0].product_name,
@@ -38,7 +84,7 @@ export const getProductById = (req, res) => {
             size: data[0].size,
             manufacturing_cost: manufacturingCost
         }]
-        
-        return res.status(200).json(newData); // Retorna o resultado encontrado com novo valor de manufacturing_cost
+
+        return res.status(200).json(newData);
     });
 };
